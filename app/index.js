f35a7dea1bc46091ff2b8a2801d858b2c5b0b209
Stephanie Nemeth <traumverloren@gmail.com>
2016-03-27 17:49:30 +0200

    set up routing

--- app/index.js
+++ app/index.js
@@ -1,15 +1,12 @@
+
+
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var HelloWorld = React.createClass({
-  render: function () {
-    return (
-      <div> Hello People </div>
-    )
-  }
-});
+
 
 ReactDOM.render(
-  <HelloWorld />,
+  routes,
   document.getElementById('app')
 );
