7135023c1440777d284bb51ff87ff7ce44a0079d
Stephanie Nemeth <traumverloren@gmail.com>
2016-03-27 20:19:10 +0200

    setup promptcontainer and prompt component

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,7 @@ var Main = React.createClass({
   render: function () {
     return (
       <div className="main-container">
-        Hello from Main!
+        
         {this.props.children}
       </div>
     )
